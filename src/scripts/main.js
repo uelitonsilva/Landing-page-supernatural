ce855c4559e4c911113f55d349bf2a761903bb85
ueliton silva <ueliton45@hotmail.com>
2024-04-21 21:41:21 -0300

    finalizando projeto

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+        if(posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        }else {
+            exibeElementosDoHeader();
+        }
+    })
+
+    function ocultaElementosDoHeader() {
+        const header =document.querySelector('header');
+        header.classList.add('header--is-hidden');
+    }
+    
+    function exibeElementosDoHeader() {
+        const header =document.querySelector('header');
+        header.classList.remove('header--is-hidden');
+    }
+})  
